2c0ff9780de84f1393e29e228a2616320be563d3
Benjamin Tang <benjamin.tang.hz@gmail.com>
2021-09-10 19:33:06 +0800

    changed promo code link

--- components/notion/Promo.tsx
+++ components/notion/Promo.tsx
@@ -34,7 +34,11 @@ export default function Promo() {
               </p>
             </div>
             <div className="order-3 mt-2 flex-shrink-0 w-full sm:order-2 sm:mt-0 sm:w-auto">
-              <Link href={promoLink}>
+              <Link
+                href={
+                  "https://www.udemy.com/course/notion-from-head-to-toe/?couponCode=SEPTPROMO"
+                }
+              >
                 <a className="flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-indigo-600 bg-white hover:bg-indigo-50">
                   Claim your discount now
                 </a>
