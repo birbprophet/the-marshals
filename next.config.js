42178f6aa567335fe49dd6a651e395bae9b6da04
Benjamin Tang <benjamin.tang.hz@gmail.com>
2021-09-13 16:46:29 +0800

    remove plugins

--- next.config.js
+++ next.config.js
@@ -1,16 +1,6 @@
 // This file was automatically added by layer0 init.
 // You should commit this file to source control.
 const { withLayer0, withServiceWorker } = require("@layer0/next/config");
-const withPWA = require("next-pwa");
-const isProduction = process.env.NODE_ENV === "production";
-
-const _preLayer0Export = withPWA({
-  reactStrictMode: true,
-  pwa: {
-    disable: !isProduction,
-    dest: "public",
-  },
-});
 
 module.exports = (phase, config) =>
   withLayer0(
@@ -18,6 +8,5 @@ module.exports = (phase, config) =>
       // Output sourcemaps so that stack traces have original source filenames and line numbers when tailing
       // the logs in the Layer0 developer console.
       layer0SourceMaps: true,
-      ..._preLayer0Export,
     })
   );
