e12ced8cc54b351ab74a48430cc8ba3c09bb1bfc
Benjamin Tang <benjamin.tang.hz@gmail.com>
2021-11-11 14:42:04 +0800

    changed promo code

--- components/notion/Promo.tsx
+++ components/notion/Promo.tsx
@@ -8,12 +8,12 @@ export default function Promo() {
   return (
     bannerShown && (
       <div className="bg-indigo-700">
-        <div className="max-w-7xl mx-auto py-3 px-3 sm:px-6 lg:px-8">
-          <div className="flex items-center justify-between flex-wrap">
-            <div className="w-0 flex-1 flex items-center">
-              <span className="flex p-2 rounded-lg bg-indigo-800">
+        <div className="px-3 py-3 mx-auto max-w-7xl sm:px-6 lg:px-8">
+          <div className="flex flex-wrap items-center justify-between">
+            <div className="flex items-center flex-1 w-0">
+              <span className="flex p-2 bg-indigo-800 rounded-lg">
                 <SpeakerphoneIcon
-                  className="h-6 w-6 text-white"
+                  className="w-6 h-6 text-white"
                   aria-hidden="true"
                 />
               </span>
@@ -21,37 +21,37 @@ export default function Promo() {
                 <span className="md:hidden">
                   Get <span className="font-bold">43% off</span> with{" "}
                   <span className="font-mono text-lg tracking-wide py-1 px-1.5 bg-indigo-600 rounded-lg">
-                    SEPTPROMO
+                    WELCOME
                   </span>
                 </span>
                 <span className="hidden md:inline">
-                  Limited time launch promo: Get{" "}
+                  Black Friday Sale: Get{" "}
                   <span className="font-bold">43% off</span> with{" "}
                   <span className="font-mono text-lg tracking-wide py-1 px-1.5 bg-indigo-600 rounded-lg">
-                    SEPTPROMO
+                    WELCOME
                   </span>
                 </span>
               </p>
             </div>
-            <div className="order-3 mt-2 flex-shrink-0 w-full sm:order-2 sm:mt-0 sm:w-auto">
+            <div className="flex-shrink-0 order-3 w-full mt-2 sm:order-2 sm:mt-0 sm:w-auto">
               <Link
                 href={
-                  "https://www.udemy.com/course/notion-from-head-to-toe/?couponCode=SEPTPROMO"
+                  "https://www.udemy.com/course/notion-from-head-to-toe/?couponCode=WELCOME"
                 }
               >
-                <a className="flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-indigo-600 bg-white hover:bg-indigo-50">
+                <a className="flex items-center justify-center px-4 py-2 text-sm font-medium text-indigo-600 bg-white border border-transparent rounded-md shadow-sm hover:bg-indigo-50">
                   Claim your discount now
                 </a>
               </Link>
             </div>
-            <div className="order-2 flex-shrink-0 sm:order-3 sm:ml-3">
+            <div className="flex-shrink-0 order-2 sm:order-3 sm:ml-3">
               <button
                 type="button"
-                className="-mr-1 flex p-2 rounded-md hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-white sm:-mr-2"
+                className="flex p-2 -mr-1 rounded-md hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-white sm:-mr-2"
                 onClick={() => setBannerShown(false)}
               >
                 <span className="sr-only">Dismiss</span>
-                <XIcon className="h-6 w-6 text-white" aria-hidden="true" />
+                <XIcon className="w-6 h-6 text-white" aria-hidden="true" />
               </button>
             </div>
           </div>
